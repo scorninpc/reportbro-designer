4d3506332e4d0e2f46b6654b1a1510e888b12bb9
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2022-11-17 16:16:45 -0300

    internacionalização

--- src/i18n/locales.js
+++ src/i18n/locales.js
@@ -1,9 +1,11 @@
 import locale_de_de from './locale_de_de';
 import locale_en_us from './locale_en_us';
+import locale_pt_br from './locale_pt_br';
 
 let locales = {
     de_de: locale_de_de,
-    en_us: locale_en_us
+    en_us: locale_en_us,
+    pt_br: locale_pt_br
 };
 
 export default locales;
